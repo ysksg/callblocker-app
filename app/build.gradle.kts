b5396e549d744135d0df65aa7889e523159b784b
unknown <y.ksg1217@gmail.com>
2026-02-27 18:46:21 +0900

    chore: version bump to v2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "net.ysksg.callblocker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
